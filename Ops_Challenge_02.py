3931164acbe43ddcb2ed67647ba9a7e230ba59d7
tph <thousden053@outlook.com>
2023-04-18 23:10:15 +0000

    submit ch2

--- Ops_Challenge_02.py
+++ Ops_Challenge_02.py
@@ -3,10 +3,11 @@
 #Import libraries
 import os, time
 from datetime import datetime
-#Infinite loops to keep pinging target
-
+#asks user for input of ip address
 IP = input("Please enter an IP address\n")
+#set variable for timestamp
 currentTime = datetime.now()
+#Infinite loops to keep pinging target
 while True:
 #creates function for ping and status 
     def uptime(IP):
@@ -16,14 +17,15 @@ while True:
             ping_status = ("Network is active")
         else:
             ping_status = ("Network is down")
-        #waits 2 seconds before next response
-        
-
+        #outputs ping status 
         return ping_status
+    #assigns uptime function with IP variable to pingstatus2 variable
     ping_status_2 = uptime(IP)
-
+    #outputs pingstatus2 variable
     print(ping_status_2)
+    #prints time stamp at the end of each ping.
     print (currentTime)
+    #sleeps command for 2 seconds before completing the loop and running from the top.
     time.sleep(2)
 
 
